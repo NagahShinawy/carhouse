f834f05232109c368b71344e0534c05a8ef6299a
Nagah <E-n.Shinawy@lean.sa>
2021-08-03 12:13:38 +0300

    add debugger toolbar

--- config/settings/development.py
+++ config/settings/development.py
@@ -2,3 +2,15 @@
 created by Nagaj at 02/08/2021
 """
 from .base import *
+
+INSTALLED_APPS += [
+    'debug_toolbar',
+]
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
+
+MIDDLEWARE += [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]

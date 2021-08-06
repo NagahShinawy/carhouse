07aaea0754fbf961d4b8f8a04f7937a55382ac81
Nagah <E-n.Shinawy@lean.sa>
2021-08-06 17:39:38 +0300

    adding ListView to index page

--- apps/cars/views.py
+++ apps/cars/views.py
@@ -1,14 +1,14 @@
 import logging
-from django.views.generic import TemplateView
+from django.views.generic import ListView, TemplateView
 from apps.teams.models import Team
 
 
-class IndexView(TemplateView):
+class IndexView(ListView):
     template_name = "cars/index.html"
     MEMBERS_LIMIT = 4
+    model = Team
 
     def get_context_data(self, **kwargs):
-        # kwargs["member"] = Team.objects.first_member_by_id(pk=257)
         kwargs["team"] = Team.objects.limit_team_by(limit=self.MEMBERS_LIMIT)
         logging.info(f"members team [{kwargs['team']}]")
         return kwargs

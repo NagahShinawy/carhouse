291d810082e83a2017aecaa2d15802ab89806534
Nagah <E-n.Shinawy@lean.sa>
2021-08-07 18:22:36 +0300

    custom car admin model

--- apps/cars/admin.py
+++ apps/cars/admin.py
@@ -8,6 +8,9 @@ from apps.core.admin.mixin import ThumbnailMixin
 class CarAdmin(admin.ModelAdmin, ThumbnailMixin):
     list_display = [field.name for field in Car._meta.fields]
     list_display += ["thumbnail"]
+    list_filter = ("year", "model")
+    list_per_page = 5
+    list_display_links = ("id", "car_title", "state")
 
     def thumbnail(self, obj, *args):
         return super(CarAdmin, self).thumbnail(obj)
--- apps/teams/admin.py
+++ apps/teams/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-from django.utils.html import format_html
-
 from .models import Team
 from apps.core.admin.mixin import ThumbnailMixin
 
@@ -21,8 +19,6 @@ class TeamAdmin(admin.ModelAdmin, ThumbnailMixin):
     list_per_page = 10
     search_fields = ("id", "emp_id", "fname", "lname", "position")
     list_filter = ("created", "modified", "position")
-    # raw_id_fields = ("emp_id",)
-    # list_select_related = ("nationality",)
 
     def thumbnail(self, obj, *args):
         return super().thumbnail(obj, is_link=True)

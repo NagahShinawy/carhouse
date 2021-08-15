8f04bfb27411d7be5cfa944f40c244328e3be86d
Nagah <E-n.Shinawy@lean.sa>
2021-08-15 11:15:11 +0300

    adding dashboard to navbar

--- opensooq/urls.py
+++ opensooq/urls.py
@@ -23,7 +23,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("apps.cars.urls")),
     path("accounts/", include("apps.contacts.urls")),
-    # path("", include("apps.teams.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
